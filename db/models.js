423433d725f88b04702e231a3e531b0c7feb967b
Siry15 <131191061+Siry15@users.noreply.github.com>
2023-12-04 14:16:46 -0400

    Add files via upload

--- db/models.js
+++ db/models.js
@@ -3,14 +3,19 @@ const db = require('./connection');
 let querys = {
     getproducto: 'SELECT * FROM producto',
     getproductoID: 'SELECT * FROM producto WHERE id = ?',
+    getproductoNO: 'SELECT * FROM producto ORDER BY name ASC',
+    getproductoDE: 'SELECT * FROM producto ORDER BY description ASC',
+    getproductoCA: 'SELECT * FROM producto ORDER BY category_id ASC',
+    getproductoMO: 'SELECT * FROM producto ORDER BY model ASC',
+    getproductoPO: 'SELECT * FROM producto ORDER BY power ASC',
     getimagenID: 'SELECT * FROM imagen WHERE id = ?',
-    insertproducto: 'INSERT INTO producto (code, name, brand, model, description, price, category_id) VALUES(?, ?, ?, ?, ?, ?, ?)',
+    insertproducto: 'INSERT INTO producto (code, name, power, model, description, price, category_id) VALUES(?, ?, ?, ?, ?, ?, ?)',
     getimagen: 'SELECT * FROM imagen',
     getcategory: 'SELECT * FROM category',
     getcategoryID: 'SELECT * FROM category WHERE id = ?',
     insertimagen: 'INSERT INTO imagen (url, producto_id, destacado) VALUES(?, ?, ?)',
     insertcategory: 'INSERT INTO category(name) VALUES(?)',
-    updateproducto: 'UPDATE producto SET code = ?, name = ?, brand = ?, model = ?, description = ?, price = ?, category_id = ? WHERE id = ?',
+    updateproducto: 'UPDATE producto SET code = ?, name = ?, power = ?, model = ?, description = ?, price = ?, category_id = ? WHERE id = ?',
     updateimagen: 'UPDATE imagen SET url = ?, producto_id = ?, destacado = ? WHERE id = ?',
     updatecategory: 'UPDATE category SET name = ? WHERE id = ?',
     deleteproducto: 'DELETE FROM producto WHERE id = ?',
@@ -26,13 +31,56 @@ module.exports = {
                 resolve(rows);
             })
         })
-
-    
     },
     
-    insertproducto(code, name, brand, model, description, price, category_id){
+    getproductoNO(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getproductoNO, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    getproductoDE(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getproductoDE, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    getproductoCA(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getproductoCA, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    getproductoMO(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getproductoMO, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    getproductoPO(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getproductoPO, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    insertproducto(code, name, power, model, description, price, category_id){
         return new Promise((resolve, reject) => {
-            db.run(querys.insertproducto, [code, name, brand, model, description, price, category_id], (err) => {
+            db.run(querys.insertproducto, [code, name, power, model, description, price, category_id], (err) => {
                 if(err) reject(err);
                     resolve()
             })
@@ -49,9 +97,9 @@ module.exports = {
         })
     },
 
-    updateproducto(id, code, name, brand, model, description, price, category_id){
+    updateproducto(id, code, name, power, model, description, price, category_id){
         return new Promise((resolve, reject) => {
-            db.run(querys.updateproducto, [code, name, brand, model, description, price, category_id, id], (err) => {
+            db.run(querys.updateproducto, [code, name, power, model, description, price, category_id, id], (err) => {
                 if(err) reject(err);
                 resolve();
             })
