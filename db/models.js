b20d1d85c06dbab3c34632b8cf338162122d2b49
Siry15 <131191061+Siry15@users.noreply.github.com>
2024-02-24 20:28:34 -0400

    Add files via upload

--- db/models.js
+++ db/models.js
@@ -6,22 +6,28 @@ let querys = {
     getproductoNO: 'SELECT * FROM producto ORDER BY name ASC',
     getproductoDE: 'SELECT * FROM producto ORDER BY description ASC',
     getproductoCA: 'SELECT * FROM producto ORDER BY category_id ASC',
-    getproductoMO: 'SELECT * FROM producto ORDER BY model ASC',
-    getproductoPO: 'SELECT * FROM producto ORDER BY power ASC',
+    getproductoMO: 'SELECT * FROM producto ORDER BY lab ASC',
+    getproductoPO: 'SELECT * FROM producto ORDER BY quantity ASC',
+    getproductoPU: 'SELECT * FROM producto JOIN puntaje ON producto.id = puntaje.producto_id ORDER BY promedio DESC',
     getimagenID: 'SELECT * FROM imagen WHERE id = ?',
-    insertproducto: 'INSERT INTO producto (code, name, power, model, description, price, category_id) VALUES(?, ?, ?, ?, ?, ?, ?)',
+    insertproducto: 'INSERT INTO producto (code, name, lab, quantity, description, price, category_id) VALUES(?, ?, ?, ?, ?, ?, ?)',
     getimagen: 'SELECT * FROM imagen',
     getcategory: 'SELECT * FROM category',
     getcategoryID: 'SELECT * FROM category WHERE id = ?',
     insertimagen: 'INSERT INTO imagen (url, producto_id, destacado) VALUES(?, ?, ?)',
     insertcategory: 'INSERT INTO category(name) VALUES(?)',
-    updateproducto: 'UPDATE producto SET code = ?, name = ?, power = ?, model = ?, description = ?, price = ?, category_id = ? WHERE id = ?',
+    updateproducto: 'UPDATE producto SET code = ?, name = ?, lab = ?, quantity = ?, description = ?, price = ?, category_id = ? WHERE id = ?',
     updateimagen: 'UPDATE imagen SET url = ?, producto_id = ?, destacado = ? WHERE id = ?',
     updatecategory: 'UPDATE category SET name = ? WHERE id = ?',
     deleteproducto: 'DELETE FROM producto WHERE id = ?',
     deleteimagen: 'DELETE FROM imagen WHERE id = ?',
-    deletecategory: 'DELETE FROM category WHERE id = ?'
-
+    deletecategory: 'DELETE FROM category WHERE id = ?',
+    insertclient: 'INSERT INTO client (email, pass) VALUES(?, ?)',
+    getclient: 'SELECT * FROM client',
+    getcompra: 'SELECT * FROM compra',
+    insertcompra: 'INSERT INTO compra (cliente_id, producto_id, cantidad, total_pagado, fecha, ip_cliente) VALUES(?, ?, ?, ?, ?, ?)',
+    insertpuntaje: 'INSERT INTO calificacion (puntos, producto_id) VALUES(?, ?)',
+    getpuntaje: 'SELECT * FROM puntaje'
 }
 module.exports = {
     getproducto(){
@@ -32,7 +38,93 @@ module.exports = {
             })
         })
     },
+
+    getproductoPU(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getproductoPU, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    getpuntaje(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getpuntaje, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    insertpuntaje(puntos, producto_id){
+        return new Promise((resolve, reject) => {
+            db.run(querys.insertpuntaje, [puntos, producto_id], (err) => {
+                if(err) reject(err);
+                    resolve()
+            })
+        })
     
+    },
+
+    insertcompra(cliente_id, producto_id, cantidad, total_pagado, fecha, ip_cliente){
+        return new Promise((resolve, reject) => {
+            db.run(querys.insertcompra, [cliente_id, producto_id, cantidad, total_pagado, fecha, ip_cliente], (err) => {
+                if(err) reject(err);
+                    resolve()
+            })
+        })
+    
+    },
+
+    insertclient(email, pass){
+        return new Promise((resolve, reject) => {
+            db.run(querys.insertclient, [email, pass], (err) => {
+                if(err) reject(err);
+                    resolve()
+            })
+        })
+    
+    },
+
+    getclient(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getclient, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    getcompra(){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getcompra, (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
+    
+    insertproducto(code, name, lab, quantity, description, price, category_id){
+        return new Promise((resolve, reject) => {
+            db.run(querys.insertproducto, [code, name, lab, quantity, description, price, category_id], (err) => {
+                if(err) reject(err);
+                    resolve()
+            })
+        })
+    
+    },
+
+    getproductoID(id){
+        return new Promise((resolve, reject)=>{
+            db.all(querys.getproductoID, [id], (err,rows)=>{
+                if(err) reject(err);
+                resolve(rows);
+            })
+        })
+    },
+
     getproductoNO(){
         return new Promise((resolve, reject)=>{
             db.all(querys.getproductoNO, (err,rows)=>{
@@ -78,28 +170,9 @@ module.exports = {
         })
     },
 
-    insertproducto(code, name, power, model, description, price, category_id){
+    updateproducto(id, code, name, lab, quantity, description, price, category_id){
         return new Promise((resolve, reject) => {
-            db.run(querys.insertproducto, [code, name, power, model, description, price, category_id], (err) => {
-                if(err) reject(err);
-                    resolve()
-            })
-        })
-    
-    },
-
-    getproductoID(id){
-        return new Promise((resolve, reject)=>{
-            db.all(querys.getproductoID, [id], (err,rows)=>{
-                if(err) reject(err);
-                resolve(rows);
-            })
-        })
-    },
-
-    updateproducto(id, code, name, power, model, description, price, category_id){
-        return new Promise((resolve, reject) => {
-            db.run(querys.updateproducto, [code, name, power, model, description, price, category_id, id], (err) => {
+            db.run(querys.updateproducto, [code, name, lab, quantity, description, price, category_id, id], (err) => {
                 if(err) reject(err);
                 resolve();
             })
